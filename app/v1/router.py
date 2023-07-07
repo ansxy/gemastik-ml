edd94c4cb54730558d7e0667e882c32f06235a89
ansxy <ansarfadillah17@gmail.com>
2023-07-07 22:08:29 +0800

    refac base v1 router

--- app/v1/router.py
+++ app/v1/router.py
@@ -14,12 +14,27 @@ class Body(BaseModel):
     rainfall : float
 
 
-@router.post('/')
+@router.post('/plant-recomendation/', status_code =200)
 async def plant_recomendation(body: Body):
-    filename = '/app/core/models/plant_recomendation.pkl' # Specify the filename of the pickle file
+    filename = 'app/core/models/plant_recomendation.pkl' # Specify the filename of the pickle file
     with open(filename, 'rb') as file:
         model = pickle.load(file)
     temp = [body.nitrogen,body.phosphorous,body.potash,body.temperature,body.humidity,body.ph,body.rainfall	]
     temp_reshaped = np.array(temp).reshape(1, -1)
     predictions = model.predict(temp_reshaped)[0]
-    return {"message": "Hello World", "plan_recomendation" : predictions}
+    return {"plan_recomendation" : predictions}
+
+@router.get('/',status_code=200)
+async def root():
+    return {
+        "status" : "ok",
+        "Version" : "v1",
+        "Team" : [
+            "Ansar Fadillah",
+            "Dhafa Agusrian Putra",
+            "Muhammad Fathurrohim"
+        ],
+        "Description" : "HarvestMoon Machine Learning API",
+        "Api Prefix" : "/v1/",
+        "Api Repository" : "https://github.com/ansxy/gemastik-ml"
+    }
